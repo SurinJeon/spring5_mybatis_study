f0b5f30d3bfaa3ab650ac88827fcdfef78824ecb
SurinJeon <surin0901@naver.com>
2021-05-10 16:45:36 +0900

    commit

---
+++ Scripts/02.grant.sql
@@ -0,0 +1,7 @@
+drop user if exists 'user_mybatis_study'@'localhost';
+
+grant all
+   on mybatis_study.*
+   to 'user_mybatis_study'@'localhost' identified by 'rootroot';
+   
+  
---
+++ Scripts/05.test.sql
@@ -0,0 +1,5 @@
+select * from addresses;
+select * from students;
+select * from tutors;
+select * from courses;
+select * from course_enrollment;
